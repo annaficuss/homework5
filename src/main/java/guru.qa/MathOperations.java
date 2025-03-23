b54af263893af7063b289d341a8a3f2e41066fc6
Anastasia Afanaseva <annaficuss@gmail.com>
2025-03-23 14:42:40 +0300

    add number range and overflow examples

--- src/main/java/guru.qa/MathOperations.java
+++ src/main/java/guru.qa/MathOperations.java
@@ -7,22 +7,27 @@ public class MathOperations {
         int a = 6;
         int b = 9;
         double c = 5.5;
-        int d = 2147483647;
+        float d = Float.MAX_VALUE;
 
-
-        System.out.println("Задание 0");
+        System.out.println("Арифметические операции над двумя примитивами типа int: ");
         System.out.println((a - b) * 6);
         System.out.println((a + b) / 2);
 
-        System.out.println("Задание 1");
+        System.out.println("Арифметические операции над int и double в одном выражении: ");
         System.out.println((a - c) * 6);
         System.out.println((a + c) / 2);
 
-        System.out.println("Задание 2");
+        System.out.println("Логические операции ");
         System.out.println(a >= b);
         System.out.println(b == c);
 
-        System.out.println("Задание 4");
-        System.out.println(d + 1);
+        System.out.println("Диапазоны типов данных для вещественных чисел: ");
+        System.out.println("Максимальное значение float: " + Float.MAX_VALUE);
+        System.out.println("Минимальное значение float: " + Float.MIN_VALUE);
+        System.out.println("Максимальное значение double: " + Double.MAX_VALUE);
+        System.out.println("Минимальное значение double: " + Double.MIN_VALUE);
+
+        System.out.println("Получить переполнение при арифметической операции ");
+        System.out.println(d * 6);
     }
 }
